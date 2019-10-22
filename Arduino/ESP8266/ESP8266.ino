2a4ff6fb5852db467b1a6d7e6a9e1a4bbb0fe86c
bartyo <bartyo2@hotmail.fr>
2019-10-22 17:20:59 +0200

    Process x-auth-token and data from post

--- Arduino/ESP8266/ESP8266.ino
+++ Arduino/ESP8266/ESP8266.ino
@@ -13,8 +13,49 @@ const uint8_t fingerprint[20] = {0x08, 0x3B, 0x71, 0x72, 0x02, 0x43, 0x6E, 0xCA,
 
 //const char* url = "https://intense-thicket-18308.herokuapp.com";
 //const char* url = "https://cautomatix.000webhostapp.com/essai_wifi.php";
-//const char* url = "https://projsante.herokuapp.com/api/auth";
-const char* url = "https://projsante.herokuapp.com/api/relays";
+const char* url_auth = "https://projsante.herokuapp.com/api/auth";
+const char* url_relay = "https://projsante.herokuapp.com/api/relays";
+
+/***DE PROCESS.INO***/
+/***BUFFER***/
+#define NB_BUFFER_HTTP 200
+#define BLE_DATA_LONG 9
+#define NB_STACK_BLE 30
+#define NUMBER_DATA_SAVED 30
+#define ID_LENGTH 25
+char g_buffer_http[NB_BUFFER_HTTP] = {0};//ID = 2 bytes
+char g_buffer_ble[BLE_DATA_LONG] = {0};//buffer de reception ble
+short g_ble_stack_id[NB_STACK_BLE] = {0};//Stack BLE
+
+/***DEVICES***/
+typedef struct{
+  char ID[ID_LENGTH] = {0};
+  char O2 = 0;
+  char GainO2 = 0;
+  char pulse = 0;
+  char GainPulse = 0;
+  char _status = 0;//b0:working;b1:enable
+  char dataSaved = 0;//To know what is the index of data in data[][index]
+  char data[BLE_DATA_LONG][NUMBER_DATA_SAVED] = {{0},{0}};//Save of the 30 previous data
+}struct_sensor;
+
+#define NB_SENSORS 100
+struct_sensor g_sensor[NB_SENSORS];
+/***DE PROCESS.INO***/
+
+
+/***PROTOTYPES***/
+void get_token(String token_str);
+void get_id();
+int long_char(char* str);
+
+#define TOKEN_LONG 184
+const char* user = "";
+const char* password = "";
+char token[TOKEN_LONG];
+String token_str;
+long timer_relay = 0;
+bool token_done = false;
 
 ESP8266WiFiMulti WiFiMulti;
 
@@ -26,7 +67,7 @@ void setup() {
   Serial.println();
   Serial.println();
   Serial.println();
-
+  get_id();
   for (uint8_t t = 4; t > 0; t--) {
     Serial.printf("[SETUP] WAIT %d...\n", t);
     Serial.flush();
@@ -34,8 +75,9 @@ void setup() {
   }
 
   WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("TP-LINK_0330", "14537486");//"Go Concept", "Grenoble2018"
-  //WiFiMulti.addAP("Go Concept", "Grenoble2018");//"Go Concept", "Grenoble2018"
+  //WiFiMulti.addAP("TP-LINK_0330", "14537486");//"Go Concept", "Grenoble2018"
+  WiFiMulti.addAP("Go Concept", "Grenoble2018");//"Go Concept", "Grenoble2018"
+  timer_relay = millis();
 }
 
 void loop() {
@@ -47,47 +89,157 @@ void loop() {
     client->setFingerprint(fingerprint);
 
     HTTPClient https;
-
-    Serial.print("[HTTPS] begin...\n");
-    if (https.begin(*client, url)) {  // HTTPS https://projsante.herokuapp.com/api/users ; https://cautomatix.000webhostapp.com/essai_wifi.php
-
-      //Serial.print("[HTTPS]"+url+"  POST...\n");
-      // start connection and send HTTP header
-      //https.addHeader("Content-Type", "application/json");
-      https.addHeader("x-auth-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWQ4ZTU5ZTc0MTllZDYwYTk0MzRjYTc3In0sImlhdCI6MTU3MDcxMzE4MywiZXhwIjoxNjAyMjQ5MTgzfQ.9JP_zqftCCD0C9WTAN-AkdAfm53Y_uFFlPURpqv2feQ");
-      //https.addHeader("upgrade-insecure-requests", "1");//application/x-www-form-urlencoded
-      //https.addHeader("Accept", "*/*");
-      //String data = "{ \"firstname\" : \"yoann\", \"email\" : \"yb@gc.com\", \"password\" : \"goconcept\" }";
-      //String data = "{ \"email\" : \"test@test.com\" , \"password\" : \"12345\" }";
-      //int httpCode = https.POST("hey="+data);
-      //int httpCode = https.POST(data);
-      int httpCode = https.GET();
-
-      // httpCode will be negative on error
-      if (httpCode > 0) 
-      {
-        // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTPS] POST... code: %d\n", httpCode);
-        String payload = https.getString();
-          Serial.println(payload);
-        // file found at server
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) 
+    if(millis() - timer_relay > 10000)//request relays
+    {
+      timer_relay = millis();
+      Serial.print("[HTTPS] Relay...\n");
+      if (https.begin(*client, url_relay)) // HTTPS https://projsante.herokuapp.com/api/auth
+      {  
+        https.addHeader("x-auth-token",token);
+        int httpCode = https.GET();
+      
+        if (httpCode > 0) 
         {
-          String payload = https.getString();
-          Serial.println(payload);
+          // HTTP header has been send and Server response header has been handled
+          Serial.printf("[HTTPS] POST... code: %d\n", httpCode);
+          // file found at server
+          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) 
+          {
+            String payload = https.getString();
+            Serial.println(payload);
+          }
         }
-      } 
-      else 
-      {
-        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+        else 
+        {
+          Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+        }
+        https.end();
       }
-
-      https.end();
-    } else {
-      Serial.printf("[HTTPS] Unable to connect\n");
+      else
+      {
+        Serial.printf("[HTTPS] Unable to connect\n");
+      }
+    }
+    if(!token_done)
+    {
+      Serial.print("[HTTPS] begin...\n");
+      if (https.begin(*client, url_auth)) // HTTPS https://projsante.herokuapp.com/api/auth
+      {  
+        https.addHeader("Content-Type","application/json");
+        String data = "{ \"email\" : \"test@test.com\" , \"password\" : \"12345\" }";
+        int httpCode = https.POST(data);
+      
+        if (httpCode > 0) 
+        {
+          // HTTP header has been send and Server response header has been handled
+          Serial.printf("[HTTPS] POST... code: %d\n", httpCode);
+          //get_token(https.getString());
+          //Serial.println(token);
+          // file found at server
+          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) 
+          {
+            token_done = true;
+            get_token(https.getString());
+            Serial.println(token);
+          }
+        }
+        else 
+        {
+          Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+        }
+        https.end();
+      }
+      else
+      {
+        Serial.printf("[HTTPS] Unable to connect\n");
+      }
+      delay(1000);
     }
   }
+}
 
-  Serial.println("Wait 10s before next round...");
-  delay(10000);
+void get_token(String token_str)
+{
+  for(int i=0;i<183;i++)
+  {
+    token[i] = token_str[i+10];
+  }
+}
+
+void get_id()
+{
+  char *data = "[{\"_id\":\"5d9b138104f4a117aa9f6b09\"},{\"_id\":\"5d9b138104f4a117aa9f6b09\"}]";
+  char id[26];
+  int j=0;
+  bool debut = false;
+  for(int i=0;i<long_char(data);i++)
+  {
+    if(data[i] == ':')
+    {
+      debut = true;
+      j=0;
+    }
+    if(j == 24)
+    {
+      id[j] = '\0';
+      Serial.println();
+      j=0;
+      debut = false;
+    }
+    if(debut && data[i]!=':' && data[i]!='"')
+    {
+      id[j] = data[i];
+      Serial.print(data[i]);
+      j++; 
+    }
+  }
+  Serial.println(id);
+}
+
+void set_id_sensor(char* _id)
+{
+  int a = 0;
+  for(int i=0;i<NB_SENSORS;i++)
+  {
+    if(g_sensor[i].ID != 0)
+    {
+      for(int j=0;j<ID_LENGTH;j++)
+      {
+        if(g_sensor[i].ID[j] == _id[j])
+        {
+          a++;
+        }
+      }
+      if(a == ID_LENGTH)
+      {
+        //Sensor already exist
+      }
+      else
+      {
+        //Sensor does not exist : add one
+        for(int k=0;k<NB_SENSORS;k++)
+        {
+          if(g_sensor[k].ID == 0)
+          {
+            for(int l=0;l<ID_LENGTH;l++)
+            {
+              g_sensor[k].ID[l] = _id[l];
+            }
+            
+            _id = {0};
+          }
+        }
+      }
+    }
+  }
+}
+
+int long_char(char* str)
+{
+  int i=0;
+  while(str[i] != 0)
+  {
+    i++;
+  }
+  return i;
 }
